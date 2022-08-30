ad3fee42b966af0f15655ed2995ee2ab7e61289d
curso.ads <curso.ads@LAB06-27>
2022-08-29 21:26:57 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Aluno.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Aluno
+    {
+        //declaraçao de atributo
+        string nome;
+        string cpf;
+        string endereco;
+        int ra;
+        //declaraçao de metodos getter e setter
+         public void SetNome(String nome)
+        {
+            this.nome = nome;
+        }
+        public string GetNome()
+        {
+            return this.nome;
+        }
+        public void SetCpf(String cpf)
+        {
+            this.cpf = cpf;
+        }
+        public string GetCpf()
+        {
+            return this.cpf;
+        }
+        public void SetEndereco(String endereco)
+        {
+            this.endereco = endereco;
+        }
+        public string GetEndereco()
+        {
+            return this.endereco;
+        }
+        public void SetRa(int ra)
+        {
+            this.ra = ra;
+        }
+        public int GetRa()
+        {
+            return this.ra;
+        }
+    }
+}
